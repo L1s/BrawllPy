881cb179df4b0eec1aa371cc7dc4adc687b40c8f
Admin <maratlis88@gmail.com>
2019-01-09 22:03:39 +0200

    life bar and death effects

--- platformer.py
+++ platformer.py
@@ -8,11 +8,12 @@
 import xml.etree.ElementTree as ET
 from math import sin, radians
 from tilengine import *
-from sound import Sound
+#from sound import Sound #mac/linux sound 
+import winsound # windows sound
 
 
 # constants
-WIDTH = 640
+WIDTH = 680
 HEIGHT = 360
 ASSETS_PATH = "assets"
 SKY_COLORS = (Color.fromstring("#78D7F2"), Color.fromstring("#E2ECF2"))
@@ -90,10 +91,9 @@ class Item(object):
 			if self.type is Item.Eagle:
 				# print("Trying to spawn eagle")
 				Eagle(self, self.x, self.y - Eagle.size[1])
-			elif self.type is 4:	#### Why 4 ??? But its works :) Spanded alot of time to get fucking bug 
+			elif self.type is 4:	#### Why 4 ? But its works :) Spanded alot of time to get fucking bug 
 				# print("Trying to spawn cristal")
 				Cristal(self, self.x, self.y - Cristal.size[1])
-
 
 class Actor(object):
 	""" Generic active game entity base class """
@@ -121,23 +121,16 @@ class Actor(object):
 
 class Player(Actor):
 	""" main player entity """
-	size = (24, 36)
-	# xspeed_delta = 12
-	# xspeed_limit = 200
-	# yspeed_delta = 10
-	# yspeed_limit = 350
-	# jspeed_delta = 5
+	size = (40, 48)
 	xspeed_delta = 30
 	xspeed_limit = 200
 	yspeed_delta = 20
 	yspeed_limit = 300
 	jspeed_delta = 30
 
-
-
 	def __init__(self):
 		if type(self).spriteset is None:
-			type(self).spriteset = Spriteset.fromfile("hero_test3")
+			type(self).spriteset = Spriteset.fromfile("Lady_Sia")
 
 		Actor.__init__(self, None, 70, -36)
 		self.respowned = False
@@ -156,30 +149,39 @@ class Player(Actor):
 		self.immunity = 0
 		self.rectangle = Rectangle(0, 0, self.width, self.height)
 		self.palettes = (self.spriteset.palette, Palette.fromfile("hero_alt.act"))
-
+		self.healthBar = Healthbar()
+		self.lifeBar = Lifebar()
+		self.lives = 5
+		self.frame = 0
+		self.exploid_x = 0
+		self.exploid_y = 0
 
 	def set_idle(self):
-		""" sets idle state, idempotent """
-		
-		if self.state is not State.Idle:
+		""" sets idle state, idempotent """	
 
-			if self.weaponPicked:
-				seq = "seq_idle_arm"
-			else:
-				seq = "seq_idle"
+		if self.state is not State.Idle:
+			# if self.weaponPicked:
+			# 	seq = "seq_idle_arm"
+			# else:
+			# 	seq = "seq_idle"
+			seq = "seq_idle_sia"
 				
 			self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences[seq], 0)
 			self.state = State.Idle
 			self.xspeed = 0
 
+						
 
 	def set_running(self):
 		""" sets running state, idempotent """
 		if self.state is not State.Run:
-			if self.weaponPicked:
-				seq = "seq_run_arm"
-			else:
-				seq = "seq_run"
+		# 	if self.weaponPicked:
+		# 		seq = "seq_run_arm"
+		# 	else:
+		# 		seq = "seq_run"
+
+			seq = "seq_run_sia"
+				
 			self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences[seq], 0)
 			self.state = State.Run
 
@@ -187,15 +189,18 @@ class Player(Actor):
 		""" sets jump state, idempotent """
 	
 		if self.state is not State.Jump:
-			self.yspeed = -350
-			if self.weaponPicked:
-				seq = "seq_jump_arm"
-			else:
-				seq = "seq_jump"
+			self.yspeed = -400
+			# if self.weaponPicked:
+			# 	seq = "seq_jump_arm"
+			# else:
+			# 	seq = "seq_jump"
+
+			seq = "seq_jump_sia"
+
 			self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences[seq], 0)
 			self.state = State.Jump
 			self.medium = Medium.Air
-			sounds.play("jump", 0)
+			# sounds.play("jump", 0)			
 
 	def set_bounce(self):
 		""" bounces on top of an enemy """
@@ -218,7 +223,9 @@ class Player(Actor):
 		self.animation.disable()
 		self.sprite.set_picture(12)
 		self.immunity = 90
-		sounds.play("hurt", 0)
+		sounds.play("kick", 0)
+		#######################################################################################
+		self.healthBar.decreaseHp()
 
 	##########################################
 	def set_weapon(self, direction):
@@ -226,7 +233,6 @@ class Player(Actor):
 		self.direction = direction
 		self.weaponPicked = True
 	###########################################	
-
 
 	def update_direction(self):
 		""" updates sprite facing depending on direction """
@@ -245,7 +251,7 @@ class Player(Actor):
 
 	def update_floor(self):
 		""" process input when player is in floor medium """
-		if not world.freezePlayers:	
+		if not world.freezePlayer:	
 
 			if window.get_input(Input.RIGHT) and self.xspeed < Player.xspeed_limit:
 				self.xspeed += self.xspeed_delta
@@ -264,7 +270,6 @@ class Player(Actor):
 			
 			
 			if window.get_input(Input.A):
-				# print("Jump key pressed from the floor")
 				self.jump_counter = 0 ####
 				self.waitForFloor = False
 				if self.jump is not True:
@@ -276,25 +281,28 @@ class Player(Actor):
 
 	def update_air(self):
 		""" process input when player is in air medium """		
-		if not world.freezePlayers:	
+		if not world.freezePlayer:	
 
 			if window.get_input(Input.RIGHT) and self.xspeed < Player.xspeed_limit:
 				self.xspeed += self.jspeed_delta
 			elif window.get_input(Input.LEFT) and self.xspeed > -Player.xspeed_limit:
 				self.xspeed -= self.jspeed_delta
+		
 
 			
-			if window.get_input(Input.A) and self.yspeed > 0:
+			if window.get_input(Input.A) and self.yspeed > 0 and self.jump_counter < 3:
 				self.jump_counter += 1
 				if self.jump_counter < 3:
 					self.state = State.Idle
 				else:
 					self.state = State.Jump
-				self.sprite.set_picture(13)
+				# if not self.weaponPicked:	
+				# 	self.sprite.set_picture(22)
+				# else:
+				# 	self.sprite.set_picture(22)	
 				player.set_jump()	
 		
 		# return True	 
-
 
 	def check_left(self, x, y):
 		""" checks/adjusts environment collision when player is moving to the left """
@@ -371,6 +379,12 @@ class Player(Actor):
 
 		# adjust to ground
 		if ground is True:
+			########################
+			if len(list(filter(lambda e: isinstance(e, StartAnimation.__class__), actors))) == 0: # checks if StartAnimation in actors list	
+				world.freezePlayer = True
+			else:	
+				world.freezePlayer = False
+			###################################
 			self.yspeed = 0
 			if self.medium is Medium.Air:
 				self.medium = Medium.Floor
@@ -410,14 +424,46 @@ class Player(Actor):
 			Effect(actor.x, actor.y, spriteset_vanish, seq_pack.sequences["seq_vanish"])
 			sounds.play("pickup", 1)
 			self.set_jump()	
+
+	def check_death(self):	
+		if self.y > HEIGHT + player.size[1]/2:
+			self.exploid_x = int(self.x)
+			self.exploid_y = HEIGHT - DeathAnimation.size[0]
+			self.respowned = True
+			self.state = State.Undefined
+			self.direction = Direction.Right
+			self.xspeed = 0
+			self.yspeed = 0
+			self.weaponPicked = False
+			# self.set_idle()
+			self.immunity = 90
+			self.lives -= 1
+			self.x = 70
+			self.y = -120
+			self.lifeBar.decreaseLifes()
+			self.healthBar.encreaseHp()	
+
+	def set_death(self):		
+		self.exploid_x = int(self.x)
+		self.exploid_y = int(self.y - DeathAnimation.size[1]/2)
+		self.respowned = True
+		self.state = State.Undefined
+		self.direction = Direction.Right
+		self.xspeed = 0
+		self.yspeed = 0
+		self.weaponPicked = False
+		# self.set_idle()
+		self.immunity = 90
+		self.lives -= 1
+		self.x = 70
+		self.y = -120					
 	#################################		
 
 	def update(self):
 		""" process input and updates state once per frame """
+		world.freezePlayer = False
 		oldx = self.x
 		oldy = self.y
-		# print("self.state = %d" % self.state)
-		# print("self.medium = %d" % self.medium)
 		# update immunity
 		if self.immunity is not 0:
 			pal_index0 = (self.immunity >> 2) & 1
@@ -426,7 +472,7 @@ class Player(Actor):
 			if self.immunity is 0:
 				pal_index1 = 0
 			if pal_index0 != pal_index1:
-				self.sprite.set_palette(self.palettes[pal_index1])
+				self.sprite.set_palette(self.palettes[pal_index1])		
 
 		# update sprite facing
 		self.update_direction()
@@ -434,6 +480,7 @@ class Player(Actor):
 		# user input: move character depending on medium
 		
 		if self.medium is Medium.Floor:
+			# world.freezePlayer = False	
 			self.update_floor()
 
 		elif self.medium is Medium.Air:
@@ -450,7 +497,11 @@ class Player(Actor):
 			self.x = 0.0
 		elif self.x > world.foreground.width - self.width:
 			self.x = world.foreground.width - self.width
-			
+
+		########################
+		self.check_death()
+									
+		#######################	
 
 		# check and fix 4-way collisions depending on motion direction
 		intx = int(self.x)
@@ -459,7 +510,7 @@ class Player(Actor):
 			self.check_top(intx, inty)
 		elif self.yspeed >= 0:
 			self.check_bottom(intx, inty)
-		if self.xspeed < 0:
+		if self.xspeed < -10:
 			self.check_left(intx, inty)
 		elif self.xspeed > 0:
 			self.check_right(intx, inty)
@@ -470,36 +521,146 @@ class Player(Actor):
 			self.rectangle.update_position(int(self.x), int(self.y))
 			self.sprite.set_position(int(self.x) - world.x, int(self.y))
 		####################
-		# if self.y > oldy:	
-		# 	self.animation.set_sprite_animation(0, seq_pack.sequences["seq_jump"], 0)
-		# elif self.state is  State.Idle:
-		# 	self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_idle"], 0)
 		return True
 ################################################################################################
+
+class DeathAnimation(Actor):
+
+	size = (100, 108)
+	spriteset = None
+		
+	def __init__(self, x, y):
+		
+		if type(self).spriteset is None:
+			type(self).spriteset = Spriteset.fromfile("effect_boom")
+		self.frame = 0	
+		Actor.__init__(self, None, 100, HEIGHT)			
+		self.sprite.set_position(100, HEIGHT)
+		self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_boom"], 0)
+		self.flag = False
+
+
+	def update(self):	
+		""" Update once per frame """	
+		if player.respowned:
+			self.sprite.set_position(player.exploid_x - player.size[0], player.exploid_y)
+			self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_boom"], 0)
+			sounds.play("exlosion", 1)
+			player.respowned = False		
+			self.flag = True
+		if self.flag:
+			self.frame += 1
+		if self.frame >= 118:
+			self.flag = False
+			self.frame = 0
+			self.sprite.set_position(100, HEIGHT)
+			
+		return True		
 
 class StartAnimation(Actor):
 
 	size = (300, 150)
 	spriteset = None
-
 		
 	def __init__(self):
 		if type(self).spriteset is None:
 			type(self).spriteset = Spriteset.fromfile("effect_start")
 		self.frame = 0
 		self.layer = engine.layers[1]
-		Actor.__init__(self, None, 120, 50)
+		Actor.__init__(self, None, 140, 50)
 		self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_start"], 0)
-		self.sprite.set_position(120, 50)
+		self.sprite.set_position(140, 50)
+		sounds.play("fight", 3)
 
 	def update(self):	
 		""" Update once per frame """
-		self.frame+= 1
+		self.frame += 1
 		if self.frame >= 305 or not self.animation.get_state():
 			return False
 		return True				
 
+class Healthbar(Actor):
+
+	size = (144, 70)
+	partition = 12
+	spriteset = None
 		
+	def __init__(self):
+		if type(self).spriteset is None:
+			type(self).spriteset = Spriteset.fromfile("hp")
+		self.hpStage = self.partition
+		self.frame = 0
+		self.layer = engine.layers[1]
+		Actor.__init__(self, None, 0, 0)
+		self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_hpUp"], 0)
+		self.sprite.set_position(0, 0)
+
+
+	def decreaseHp(self):
+		seq = "seq_hpDown" 
+		if self.hpStage > 1:
+			seq = seq + str(self.hpStage)
+			# print(seq)
+			self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences[seq], 0)
+			self.hpStage -= 1
+			self.frame -= 50
+		else:
+			player.set_death()
+			player.lifeBar.decreaseLifes()
+			self.hpStage = self.partition
+			# self.sprite.set_picture(self.hpStage)	
+			self.encreaseHp()
+		return
+
+	def encreaseHp(self):
+		self.frame = 0	
+		self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_hpUp"], 0)
+		self.hpStage = self.partition
+
+	def update(self):	
+		""" Update once per frame """
+		if self.frame >= 190:
+			self.animation.disable()
+			self.sprite.set_picture(self.hpStage)
+		else:	
+			self.frame += 1	
+		return True
+	
+class Lifebar(Actor):
+
+	size = (15, 15)
+	spriteset = None
+		
+	def __init__(self):
+		if type(self).spriteset is None:
+			type(self).spriteset = Spriteset.fromfile("nums")
+		self.lifeStage = 0
+		self.frame = 0
+		self.layer = engine.layers[1]
+		Actor.__init__(self, None, 130, 25)
+		self.animation.set_sprite_animation(self.sprite.index, seq_pack.sequences["seq_life"], 0)
+		self.sprite.set_position(130, 25)
+
+
+	def decreaseLifes(self):
+		""" decreases num of lifes """
+		self.lifeStage += 1
+		self.sprite.set_picture(self.lifeStage)
+		print(self.lifeStage)
+		if self.lifeStage == 5:
+			# player.healthBar.sprite.disable()
+			# player.lifeBar.sprite.disable()
+			actors.remove(player)	
+		return
+
+	def update(self):	
+		""" Update once per frame """
+		if self.frame >= 150:
+			self.animation.disable()
+			self.sprite.set_picture(self.lifeStage)
+		else:	
+			self.frame += 1	
+		return True		
 ################################################################################################
 class Cristal(Actor):
 	"""Flying cristal """
@@ -525,7 +686,7 @@ class Cristal(Actor):
 		screen_x = self.x - world.x
 
 		if self.direction is Direction.Left:
-			if screen_x < 20:
+			if screen_x < 10:
 				self.direction = Direction.Right
 				self.xspeed = -self.xspeed
 				self.sprite.set_flags(Flags.FLIPX)
@@ -533,7 +694,7 @@ class Cristal(Actor):
 				for point in self.collision_points:
 					player.check_pick_cristal(self.x, self.y + point, self.direction)
 		else:
-			if screen_x > 600:
+			if screen_x > 650:
 				self.direction = Direction.Left
 				self.xspeed = -self.xspeed
 				self.sprite.set_flags(0)
@@ -542,10 +703,8 @@ class Cristal(Actor):
 					player.check_pick_cristal(self.x, self.y + point, self.direction)
 		self.sprite.set_position(screen_x, self.y)
 		return True			
-
 ################################################################################################
 			
-
 class Eagle(Actor):
 	""" Flying enemy """
 	size = (40, 40)
@@ -567,8 +726,8 @@ class Eagle(Actor):
 		self.x += self.xspeed
 		self.y = self.base_y + int(sin(radians(self.frame*4))*15)
 		self.frame += 1
-		if self.frame is 10:
-			sounds.play("eagle", 3)
+		# if self.frame is 10:
+		# 	sounds.play("eagle", 3)
 		# if self.frame < 200:
 		# 	screen_x = world.x + 590
 		screen_x = self.x - world.x
@@ -576,23 +735,24 @@ class Eagle(Actor):
 		# 	screen_x = world.x + 590 + (self.x - world.x)
 
 		if self.direction is Direction.Left:
-			if screen_x < 10:
+			if screen_x < 180: #0
 				self.direction = Direction.Right
 				self.xspeed = -self.xspeed
 				self.sprite.set_flags(Flags.FLIPX)
-				sounds.play("eagle", 3)
-			# else:
-			# 	for point in self.collision_points:
-			# 		player.check_hit(self.x, self.y + point, self.direction)
+				if not StartAnimation:
+					sounds.play("eagle", 3)
+			else:
+				for point in self.collision_points:
+					player.check_hit(self.x, self.y + point, self.direction)
 		else:
-			if screen_x > 590:
+			if screen_x > 460: # 640
 				self.direction = Direction.Left
 				self.xspeed = -self.xspeed
 				self.sprite.set_flags(0)
-				sounds.play("eagle", 3)
-			# else:
-			# 	for point in self.collision_points:
-			# 		player.check_hit(self.x + self.size[0], self.y + point, self.direction)
+				# sounds.play("eagle", 3)
+			else:
+				for point in self.collision_points:
+					player.check_hit(self.x + self.size[0], self.y + point, self.direction)
 		self.sprite.set_position(screen_x, self.y)
 		return True		
 
@@ -610,6 +770,10 @@ class Effect(Actor):
 			return False
 		return True
 
+#################################
+
+##################################
+
 class World(object):
 	""" world/play field entity """
 	def __init__(self):
@@ -623,8 +787,9 @@ class World(object):
 		self.objects = load_objects("assets/test.tmx", "Eagle", 973)
 		engine.set_background_color(self.background.tilemap)
 		actors.append(self)
-		self.freezePlayers = True
-		actors.append(StartAnimation())
+		self.freezePlayer = True
+		self.flagStartAnimation = True
+		
 
 	def pick_gem(self, tiles_list):
 		""" updates tilemap when player picks a gem """
@@ -638,10 +803,10 @@ class World(object):
 				break
 		del tile
 
+
 	def update(self):
 		""" updates world state once per frame """
 		oldx = self.x
-
 		if player.x < 240:
 			self.x = 0
 		else:
@@ -656,7 +821,15 @@ class World(object):
 
 		# spawn new entities from object list
 		for item in self.objects:
-			item.try_spawn(self.x)	
+			item.try_spawn(self.x)
+
+		##############
+		if self.flagStartAnimation and player.healthBar.frame == 100:
+			actors.append(StartAnimation())
+			self.flagStartAnimation = False
+
+		# if player.state is State.Undefined:
+		# 	self.freezePlayer = True
 
 		return True
 
@@ -691,6 +864,26 @@ def raster_effect(line):
 	elif line == 256:
 		world.background.set_position(world.x//2, 0)
 
+#####################
+class Sound(object):
+	""" Manages sound effects """
+	def __init__(self, path):		
+		self._sounds = dict()
+		print(path)
+		if path is None:
+			self.path = "./"
+		else:
+			self.path = path + "/"
+
+				
+	def load(self, name, filePath):
+		self._sounds[name] = str(self.path + filePath)
+		
+
+	def play(self, name, channel):
+		winsound.PlaySound(self._sounds[name], winsound.SND_ASYNC)	
+#####################
+
 # init engine
 engine = Engine.create(WIDTH, HEIGHT, 2, 32, 32)
 engine.set_load_path("assets")
@@ -699,6 +892,7 @@ engine.set_load_path("assets")
 spriteset_vanish = Spriteset.fromfile("effect_vanish")
 spriteset_death = Spriteset.fromfile("effect_death")
 spriteset_start = Spriteset.fromfile("effect_start")
+spriteset_boom = Spriteset.fromfile("effect_boom")
 
 # load sequences
 seq_pack = SequencePack.fromfile("sequences.sqx")
@@ -707,19 +901,34 @@ tiles_info = (TileInfo(), TileInfo(), TileInfo(), TileInfo())
 # set raster callback
 engine.set_raster_callback(raster_effect)
 
-actors = list()		# list that contains every active game entity
-world = World()		# world/level entity
-player = Player()   # player entity
-
-
-
-# Sound effects
-sounds = Sound(4, "assets")
+##
+sounds = Sound("assets")
 sounds.load("jump", "jump.wav")
 sounds.load("crush", "crunch.wav")
 sounds.load("pickup", "pickup.wav")
 sounds.load("hurt", "hurt.wav")
 sounds.load("eagle", "vulture.wav")
+sounds.load("fight", "123fight.wav")
+sounds.load("exlosion", "explosion.wav")
+sounds.load("kick", "kick.wav")
+
+##
+actors = list()		# list that contains every active game entity
+world = World()		# world/level entity
+player = Player()   # player entity
+# DeathAnimation(200,200)
+actors.append(DeathAnimation(200,200))
+
+
+# Sound effects
+# sounds = Sound(4, "assets")
+# sounds.load("jump", "jump.wav")
+# sounds.load("crush", "crunch.wav")
+# sounds.load("pickup", "pickup.wav")
+# sounds.load("hurt", "hurt.wav")
+# sounds.load("eagle", "vulture.wav")
+##############
+
 
 
 
@@ -728,7 +937,12 @@ window = Window.create()
 while window.process():
 	for actor in actors:
 		# if actor.__class__ != StartAnimation:
+		# if actor.__class__ == DeathAnimation:
+		# 	print(actor.__class__)
 		if not actor.update():
-			if actor.__class__ == StartAnimation:
-				world.freezePlayers = False
+			# if actor.__class__ == DeathAnimation:
+			# 	print("not update")
+			# if actor.__class__ == StartAnimation:
+			# 	world.freezePlayer = False
+			
 			actors.remove(actor)
